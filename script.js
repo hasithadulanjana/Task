90dcca9ac4484de5253b69a6c402efdd7760f980
Hasitha Dulanjana <hasithapalihena100@gmail.com>
2019-11-27 09:58:02 +1300

    Now it brings the data

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function loaddata(){
-fetch('data.json')
+fetch('data.json',{
+  //created the post method so the data is posted to the json file
+  method: 'post',
+  body: JSON.stringify()
+})
 .then(function(response){
   console.log(response);
+
   return response.json();
 
 })
 .then(function(data){
   console.log(data);
-  document.getElementById('result').innerHTML=data;
+
 })
 
 .catch(function(error){
